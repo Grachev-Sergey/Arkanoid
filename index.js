fa835ae33c6c096738f07178f23e589acf7c4f56
Sergey Grachev <serg3i-grach@yandex.ru>
2022-08-29 16:28:56 +0300

    Replaced setInterval with requestAnimationFrame, removed unnecessary event listeners, renamed variables for better readability

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const healthNumber = document.querySelector('.options-health__number');
 let difficultLevel;
 let setHealth;
 
+let isGameStarted = false;
+
 let score = 0;
 let level = 1;
 
@@ -26,66 +28,75 @@ function createGameBlock() {
   const health = document.querySelector('.health');
   const levelNumber = document.querySelector('.level');
   const scoreNumber = document.querySelector('.score');
-  
-  const leftWall = document.querySelector('.leftWall').getBoundingClientRect().width; 
-  const topWall = document.querySelector('.topWall').getBoundingClientRect().height; 
-  const rightWall = document.querySelector('.topWall').getBoundingClientRect().width; 
-  const rightWallWidth = document.querySelector('.topWall').getBoundingClientRect().height; 
-  const bottomWall = document.querySelector('.rightWall').getBoundingClientRect().height; 
+
+  const leftWall = document.querySelector('.leftWall').getBoundingClientRect().width;
+  const topWall = document.querySelector('.topWall').getBoundingClientRect().height;
+  const rightWall = document.querySelector('.topWall').getBoundingClientRect().width;
+  const rightWallWidth = document.querySelector('.topWall').getBoundingClientRect().height;
+  const bottomWall = document.querySelector('.rightWall').getBoundingClientRect().height;
   const blocks = document.querySelector('.blocks');
-  
+
   const platform = document.querySelector('.platform');
   const platformInfo = platform.getBoundingClientRect();
-  
+
   const ball = document.querySelector('.ball');
   const ballInfo = ball.getBoundingClientRect();
-  
-  document.addEventListener('keydown', startGame); 
-  document.addEventListener('keydown', movePlatformWihtBall); 
-  
+
+
   let healthPoint = setHealth;
   health.innerHTML = `Health: ${healthPoint}`;
-  
-  
+
   levelNumber.innerHTML = `Level: ${level}`;
-  
-  
+
   scoreNumber.innerHTML = `Score: ${score}`;
-  
+
   let intervalMove;
-  
-  let vx = 5, vy = -5;
-  
+
+  let ballSpeedX = 4, ballSpeedY = -4;
+
   const ballDiameter = ballInfo.height;
 
-  let posX = ballInfo.left + ballDiameter/2;
-  let posY = ballInfo.top;
-  // let ballBottomSide = posY + ballInfo.height;
+  let ballPosX = ballInfo.left + ballDiameter / 2;
+  let ballPosY = ballInfo.top;
 
-  
+
   const platformWidth = platformInfo.width;
-  const platformHeight = platformInfo.height; 
-  let platformPosX = platformInfo.left; 
-  const vPlatform = 15;
-  
+  const platformHeight = platformInfo.height;
+  let platformPosX = platformInfo.left;
+  const platformSpeed = 15;
+
+  // выбор фунцкии для события
+
+  function handleKeyDown(event) {
+    if (!isGameStarted && event.key === ' ') {
+      startGame(event);
+    }
+    if (!isGameStarted && (event.key === 'ArrowLeft' || event.key === 'ArrowRight')) {
+      movePlatformWihtBall(event);
+    }
+    if (isGameStarted && (event.key === 'ArrowLeft' || event.key === 'ArrowRight')) {
+      movePlatform(event);
+    }
+  };
+
   // создание группы блоков внутри тега blocks
-  
-  function renderBlocks () {
+
+  function renderBlocks() {
     const createBlocks = (i, j, color) => {
       return `<div class="block-row-${i} block-column-${j} block-${color} block"></div>`;
     };
-    
+
     for (let i = 1; i < 21; i++) {
       for (let j = 1; j < 6; j++) {
         let color = randomColor(1, 4);
         blocks.innerHTML += createBlocks(i, j, color);
       };
     };
-    
-    function randomColor (min, max) {
-      if (Math.floor(Math.random()*(max - min)+min) === 1) {
+
+    function randomColor(min, max) {
+      if (Math.floor(Math.random() * (max - min) + min) === 1) {
         return 'blue';
-      } else if (Math.floor(Math.random()*(max - min)+min) === 2) {
+      } else if (Math.floor(Math.random() * (max - min) + min) === 2) {
         return 'green';
       } else {
         return 'red';
@@ -93,92 +104,97 @@ function createGameBlock() {
     };
   };
   renderBlocks();
-    
+
   let block = document.querySelectorAll('.block');
-  
+
   // запуск игры, функционал движения мяча и платформы
-  
-  function startGame (event) {
-    if (event.keyCode === 32) {
-      intervalMove = setInterval(moveBall, 1000/40);
-      document.removeEventListener('keydown', startGame);
-      document.removeEventListener('keydown', movePlatformWihtBall);
-      document.addEventListener('keydown', movePlatform);
-      if (level >= difficultLevel) {
-        fallShells();
-      };
-    };
+
+  function startGame(event) {
+    isGameStarted = true;
+
+    // intervalMove = setInterval(moveBall, 20); // заменю интервал на requestAnimationFrame
+    intervalMove = requestAnimationFrame(moveBall);
+
+    if (level >= difficultLevel) {
+      fallShells();
+    }
+    
   };
-  
+
   function movePlatform(event) {
-    if(event.keyCode === 37) {
-      platformPosX -= vPlatform;
-    };
-    if(event.keyCode == 39) {
-      platformPosX += vPlatform;
-    };
+    if (event.key === 'ArrowLeft') {
+      platformPosX -= platformSpeed;
+    }
+    if (event.key === 'ArrowRight') {
+      platformPosX += platformSpeed;
+    }
     if (platformPosX < leftWall) {
       platformPosX = leftWall;
-    };
+    }
     if (platformPosX + platformWidth > rightWall) {
       platformPosX = rightWall - platformWidth;
-    };
-    platform.style.left = platformPosX + 'px';
+    }
+    platform.style.left = `${platformPosX}px`;
   };
-  
+
   function movePlatformWihtBall(event) {
-    if(event.keyCode === 37) {
-      platformPosX -= vPlatform;
-      posX -= vPlatform;
-    };
-    if(event.keyCode == 39) {
-      platformPosX += vPlatform;
-      posX += vPlatform;
-    };
+    if (event.key === 'ArrowLeft') {
+      platformPosX -= platformSpeed;
+      ballPosX -= platformSpeed;
+    }
+    if (event.key === 'ArrowRight') {
+      platformPosX += platformSpeed;
+      ballPosX += platformSpeed;
+    }
     if (platformPosX < leftWall) {
       platformPosX = leftWall;
-      posX = platformPosX + platformWidth/2 - leftWall;
-    };
+      ballPosX = platformPosX + platformWidth / 2 - leftWall;
+    }
     if (platformPosX + platformWidth + rightWallWidth > rightWall) {
       platformPosX = rightWall - rightWallWidth - platformWidth;
-      posX = rightWall - rightWallWidth - platformWidth/2;
-    };
-    platform.style.left = platformPosX + 'px';
-    ball.style.left = posX + 'px';
+      ballPosX = rightWall - rightWallWidth - platformWidth / 2;
+    }
+    platform.style.left = `${platformPosX}px`;
+    ball.style.left = `${ballPosX}px`;
   };
-  
+
   // механика перемещения шара в пространстве и контакта соприкосновения с границами и блоками
+
+  function moveBall() {
+    ballPosX += ballSpeedX; 
+    ballPosY += ballSpeedY;
+
+    if(isGameStarted) {
+      requestAnimationFrame(moveBall);
+    }
   
-  function moveBall () { 
-    posX += vx; 
-    posY += vy;
 
     //взаимодействие со стенами и платформой
 
-    if (posX < leftWall) {
-      posX = leftWall;
-      vx = -vx;
-    };
-    if (posY < topWall) {
-      posY = topWall;
-      vy = -vy;
-    };
-    if (posX + ballDiameter + rightWallWidth > rightWall) {
-      posX = rightWall - ballDiameter - rightWallWidth;
-      vx = -vx;
-    };
-    if (posY > bottomWall - platformHeight - ballDiameter) {
-      if (posX >= platformPosX - ballDiameter && posX <= platformPosX + platformInfo.width) {
-        posY = bottomWall - platformHeight - ballDiameter;
-        vy = -vy;
-      };
-    };
-    ball.style.top = posY + 'px';
-    ball.style.left = posX + 'px';
-  
+    if (ballPosX < leftWall) {
+      ballPosX = leftWall;
+      ballSpeedX = -ballSpeedX;
+    }
+    if (ballPosY < topWall) {
+      ballPosY = topWall;
+      ballSpeedY = -ballSpeedY;
+    }
+    if (ballPosX + ballDiameter + rightWallWidth > rightWall) {
+      ballPosX = rightWall - ballDiameter - rightWallWidth;
+      ballSpeedX = -ballSpeedX;
+    }
+    if (ballPosY > bottomWall - platformHeight - ballDiameter) {
+      if (ballPosX >= platformPosX - ballDiameter && ballPosX <= platformPosX + platformInfo.width) {
+        ballPosY = bottomWall - platformHeight - ballDiameter;
+        ballSpeedY = -ballSpeedY;
+      }
+    }
+    ball.style.top = `${ballPosY}px`;
+    ball.style.left = `${ballPosX}px`;
+
     // при попадании мяча в запретную зону, убрать HP или закончить игру.
-  
-    if (posY + ballDiameter > bottomWall) {
+
+    if (ballPosY + ballDiameter > bottomWall) {
       death();
     };
 
@@ -186,33 +202,33 @@ function createGameBlock() {
 
     function contactWithBlock() {
       Object.values(block).map((elem) => {
-        let elemTop = elem.getBoundingClientRect().top;
-        let elemBottom = elem.getBoundingClientRect().top + elem.getBoundingClientRect().height;
-        let elemLeft = elem.getBoundingClientRect().left;
-        let elemRight = elem.getBoundingClientRect().left + elem.getBoundingClientRect().width;
+        const elemTop = elem.getBoundingClientRect().top;
+        const elemBottom = elem.getBoundingClientRect().top + elem.getBoundingClientRect().height;
+        const elemLeft = elem.getBoundingClientRect().left;
+        const elemRight = elem.getBoundingClientRect().left + elem.getBoundingClientRect().width;
 
-        if (posY + ballDiameter/4 < elemBottom && posY > elemTop && posX > elemLeft && posX < elemRight) {
+        if (ballPosY + ballDiameter / 4 < elemBottom && ballPosY > elemTop && ballPosX > elemLeft && ballPosX < elemRight) {
           removeBlock(elem);
-          vy = -vy;
-        };
-        if (posX + ballDiameter + ballDiameter/4 > elemLeft && posX < elemRight && posY + ballDiameter/2  > elemTop && posY + ballDiameter/2 < elemBottom) {
+          ballSpeedY = -ballSpeedY;
+        }
+        if (ballPosX + ballDiameter + ballDiameter / 4 > elemLeft && ballPosX < elemRight && ballPosY + ballDiameter / 2 > elemTop && ballPosY + ballDiameter / 2 < elemBottom) {
           removeBlock(elem);
-          vx = -vx;
-        };
-        if (posX - ballDiameter/2 < elemRight && posX > elemLeft && posY + ballDiameter/2 > elemTop && posY + ballDiameter/2 < elemBottom) {
+          ballSpeedX = -ballSpeedX;
+        }
+        if (ballPosX - ballDiameter / 2 < elemRight && ballPosX > elemLeft && ballPosY + ballDiameter / 2 > elemTop && ballPosY + ballDiameter / 2 < elemBottom) {
           removeBlock(elem);
-          vx = -vx;
-        };
-         if (posY + ballDiameter/2 + ballInfo.height > elemTop && posY + ballInfo.height < elemBottom && posX > elemLeft && posX < elemRight) {
+          ballSpeedX = -ballSpeedX;
+        }
+        if (ballPosY + ballDiameter / 2 + ballInfo.height > elemTop && ballPosY + ballInfo.height < elemBottom && ballPosX > elemLeft && ballPosX < elemRight) {
           removeBlock(elem);
-          vy = -vy;
-        };
+          ballSpeedY = -ballSpeedY;
+        }
       });
     };
     contactWithBlock();
-  
+
     // если закончились блоки, создать новую группу блоков и перейти на следующий уровень 
-  
+
     if (!blocks.children.length) {
       stopGame();
       level += 1;
@@ -220,97 +236,96 @@ function createGameBlock() {
       renderBlocks();
       block = document.querySelectorAll('.block');
       contactWithBlock();
-    };
+    }
   };
-  
+
   function removeBlock(elem) {
-    if(elem.classList.contains('block-red')) {
+    if (elem.classList.contains('block-red')) {
       elem.classList.remove('block-red');
       elem.classList.add('block-green');
-      score +=1;
+      score += 1;
       scoreNumber.innerHTML = `Score: ${score}`;
     } else if (elem.classList.contains('block-green')) {
       elem.classList.remove('block-green');
       elem.classList.add('block-blue');
-      score +=1;
+      score += 1;
       scoreNumber.innerHTML = `Score: ${score}`;
     } else {
       elem.remove();
-      score +=1;
+      score += 1;
       scoreNumber.innerHTML = `Score: ${score}`;
-    };
+    }
   };
 
   // запуск снарядов
-  
+
   let intervalGenerateShells;
-    
+
   function fallShells() {
-  
-    intervalGenerateShells  = setInterval(generateShells, 3000);
-  
+
+    intervalGenerateShells = setInterval(generateShells, 3000);
+
     const shellsblock = document.querySelector('.shells-block');
-  
+
     let shells;
-    let shellsY;
-    let shellsX;
+    let shellsPosY;
+    let shellsPosX;
     let shellsHeight;
     let shellsWidth;
-    const vShellsY = 5;
-  
+    const shellsSpeedY = 6;
+
     function generateShells() {
       let shellRandomPosishon = shellPosishon(5, 95);
       shellsblock.innerHTML += `<div class = "shells" style = "left: ${shellRandomPosishon}%"></div>`;
-      function shellPosishon (min, max) {
-        return Math.floor(Math.random()*(max - min)+min);
+      function shellPosishon(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
       };
       shells = shellsblock.querySelector('.shells');
-      shellsY = shells.getBoundingClientRect().top;
-      shellsX = shells.getBoundingClientRect().left;
+      shellsPosY = shells.getBoundingClientRect().top;
+      shellsPosX = shells.getBoundingClientRect().left;
       shellsHeight = shells.getBoundingClientRect().height;
       shellsWidth = shells.getBoundingClientRect().width;
     };
 
     // механика движения снарядов
 
-    setInterval(shellsMove, 1000/50);
+    setInterval(shellsMove, 20);
     function shellsMove() {
-      shellsY += vShellsY;
+      shellsPosY += shellsSpeedY;
       if (shells) {
-        shells.style.top = shellsY + 'px';
-      };
-      if (shellsY + shellsHeight > bottomWall) {
+        shells.style.top = `${shellsPosY}px`;
+      }
+      if (shellsPosY + shellsHeight > bottomWall) {
         shells.remove();
-        shellsX = 0;
-        shellsY = 0;
-      };
-      if (shellsY + shellsHeight > bottomWall - platformHeight && shellsX > platformPosX && shellsX + shellsWidth < platformPosX + platformWidth) {
-        shellsX = 0;
-        shellsY = 0;
+        shellsPosX = 0;
+        shellsPosY = 0;
+      }
+      if (shellsPosY + shellsHeight > bottomWall - platformHeight && shellsPosX > platformPosX && shellsPosX + shellsWidth < platformPosX + platformWidth) {
+        shellsPosX = 0;
+        shellsPosY = 0;
         shells.remove();
         death();
-      };
+      }
     };
   };
-  
+
   // остановка игры 
-  
-  function stopGame () {
-    clearInterval(intervalMove);
+
+  function stopGame() {
+    isGameStarted = false;
+    // clearInterval(intervalMove);
+
     clearInterval(intervalGenerateShells);
-    document.addEventListener('keydown', startGame);
-    document.addEventListener('keydown', movePlatformWihtBall);
-    document.removeEventListener('keydown', movePlatform);
-    posX = platformPosX + platformWidth/2;
-    posY = bottomWall - platformHeight - ballDiameter; 
-    ball.style.top = posY + 'px';
-    ball.style.left = posX + 'px';
+    ballPosX = platformPosX + platformWidth / 2;
+    ballPosY = bottomWall - platformHeight - ballDiameter;
+    ball.style.top = `${ballPosY}px`;
+    ball.style.left =`${ballPosX}px`;
   };
-  
+
   // попадание мяча за поле и попадание снаряда по платформе
-  
-  function death () {
-    if (healthPoint === 1){
+
+  function death() {
+    if (healthPoint === 1) {
       gameOver();
     } else {
       stopGame();
@@ -322,9 +337,6 @@ function createGameBlock() {
   // окончание игры и вывод результатов
 
   function gameOver() {
-    clearInterval(intervalMove);
-    clearInterval(intervalGenerateShells);
-    document.removeEventListener('keydown', movePlatform);
     const numberOfLevels = document.querySelector('.number-of-levels');
     const numberOfpoints = document.querySelector('.number-of-points');
     numberOfLevels.innerHTML = level;
@@ -332,6 +344,8 @@ function createGameBlock() {
     game.classList.add('hide');
     restart.classList.remove('hide');
   };
+
+  document.addEventListener('keydown', handleKeyDown);
 };
 
 // кнопка перезапуска игры
